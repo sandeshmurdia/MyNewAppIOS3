87d9ff7542ab544438ff5dc119e9b588187608e2
Sandesh Murdia <56988281+sandeshmurdia@users.noreply.github.com>
2024-11-21 11:17:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import {name as appName} from './app.json';
 
 import zipy from 'zipy-react-native';
 
-zipy.init('6d2b9556');
+zipy.init('xxxx');
 import { withGestureCapture } from 'zipy-react-native';
 
 const MyApp = () => {
